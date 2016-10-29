e53b96d3ff6ab22cfde1d1b8ae05c6036dcc863a
Jackson Mills <aim4math@gmail.com>
2016-10-29 13:44:48 -0400

    New stuff

--- Rebirth/Assets/BehaviorAI.cs
+++ Rebirth/Assets/BehaviorAI.cs
@@ -3,18 +3,38 @@ using System.Collections;
 
 public class BehaviorAI : MonoBehaviour {
 
-    public string behavior;
+
 
     public float awarenessRadius;
+
+    private string typeOfCreature;
 
     private GameObject[] nearbyEntities;
     
     private GameObject current;
 
-	// Use this for initialization
-	void Start () {
-	
+    private string behavior;
+
+    private int power;
+
+    private int hp;
+
+    private string[] typesOfFood;
+
+    // Use this for initialization
+    void Start () {
+        behavior = "Idle";
+        typeOfCreature = "Animal";
+        hp = 100;
+        power = 100;
 	}
+
+    void initTypesOfFood()
+    {
+        typesOfFood = new string[2];
+        typesOfFood[0] = "Plant";
+        typesOfFood[1] = "Animal";
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,13 +42,41 @@ public class BehaviorAI : MonoBehaviour {
         for(int i = 0; i<nearbyEntities.GetLength(0); i++)
         {
             current = nearbyEntities[i];
-            if(current.GetComponent<"BehaviorAI"> == 1)
+            if(current.tag == "Alive")
+            {
+                AdaptBehavior(current);
+
+
+
+            } else if(Equals(current.tag, "Dead"))
             {
 
             }
         }
 
 	}
+
+    void AdaptBehavior(GameObject livingEntity)
+    {
+        if(power >= livingEntity.GetComponent<BehaviorAI>().power
+            && CanEat(livingEntity))
+        {
+            behavior = "Hostile";
+        }
+
+    }
+
+    bool CanEat(GameObject livingEntity)
+    {
+        for(int i = 0; i<typesOfFood.GetLength(0); i++)
+        {
+            if(Equals(typesOfFood[i], livingEntity.GetComponent<BehaviorAI>().typeOfCreature))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     GameObject[] GetGameObjectsWithin(float radius)
     {
