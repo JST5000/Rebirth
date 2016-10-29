106bfa3c5bc3319fcee16cf722b86786eca4372e
NathanTorrence <towence47@gmail.com>
2016-10-29 18:36:36 -0400

    Better movement changes

--- Rebirth/Assets/Scripts/PlayerController.cs
+++ Rebirth/Assets/Scripts/PlayerController.cs
@@ -29,6 +29,28 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
+        if ((Input.GetKeyDown(KeyCode.RightArrow)) || (Input.GetKeyDown(KeyCode.D)))
+        {
+            // If the user has pressed either right or D, face right
+            gameObject.GetComponent<SpriteRenderer>().sprite = right;
+        }
+        else if ((Input.GetKeyDown(KeyCode.LeftArrow)) || (Input.GetKeyDown(KeyCode.A)))
+        {
+            // If the user has pressed either left or A, face left
+            gameObject.GetComponent<SpriteRenderer>().sprite = left;
+        }
+        else if ((Input.GetKeyDown(KeyCode.UpArrow)) || (Input.GetKeyDown(KeyCode.W)))
+        {
+            // If the user has pressed either up or W, face up
+            gameObject.GetComponent<SpriteRenderer>().sprite = up;
+        }
+        else if ((Input.GetKeyDown(KeyCode.DownArrow)) || (Input.GetKeyDown(KeyCode.S)))
+        {
+            // If the user has pressed either down or S, face down
+            gameObject.GetComponent<SpriteRenderer>().sprite = down;
+        }
+
         MoveHorizontal(Input.GetAxis("Horizontal"));
         MoveVertical(Input.GetAxis("Vertical"));
 
@@ -63,13 +85,13 @@ public class PlayerController : MonoBehaviour {
         Vector2 moveVe1 = rb.velocity;
         moveVe1.x = input * speed * Time.deltaTime;
         rb.velocity = moveVe1;
-        if (input > 0)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = right;
-        } else if (input < 0)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = left;
-        }
+        //if (input > 0)
+        //{
+        //    gameObject.GetComponent<SpriteRenderer>().sprite = right;
+        //} else if (input < 0)
+        //{
+        //    gameObject.GetComponent<SpriteRenderer>().sprite = left;
+        //}
     }
 
     void MoveVertical(float input)
@@ -77,13 +99,13 @@ public class PlayerController : MonoBehaviour {
         Vector2 moveVe1 = rb.velocity;
         moveVe1.y = input * speed * Time.deltaTime;
         rb.velocity = moveVe1;
-        if (input > 0)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = up;
-        }
-        else if (input < 0)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = down;
-        }
+        //if (input > 0)
+        //{
+        //    gameObject.GetComponent<SpriteRenderer>().sprite = up;
+        //}
+        //else if (input < 0)
+        //{
+        //    gameObject.GetComponent<SpriteRenderer>().sprite = down;
+        //}
     }
 }
